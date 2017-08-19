d74cfeda96ec3d5d6fd60b94e92133b1d0113fae
Aditya Pratti <prattiaditya@gmail.com>
2017-08-18 23:39:55 -0700

    Made some changes in the main file

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,17 @@ var element=document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
 
+//To change the element on click
+
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+	marginLeft=marginLeft+1
+	img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function(){
+ var interval=setInterval(moveRight,50);
+};
+ 
+
+
